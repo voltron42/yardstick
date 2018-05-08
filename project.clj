991809dc5281d5116e7d283275633e8dbd9eab05
Daniel Johnson <dajohnson1978@gmail.com>
2018-05-08 12:44:25 -0500

    updating project.clj for clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject yardstick "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject org.clojars.voltron42/yardstick "0.1.0-SNAPSHOT"
+  :description "Yardstick is an implementation of Gauge testing in Clojure. Yardstick is built to run any valid Gauge \".spec\" test files."
+  :url "http://github.com/voltron42/yardstick"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
@@ -9,5 +9,4 @@
                  [org.clojure/data.csv "0.1.4"]
                  [markdown-clj "1.0.2"]
                  [junit/junit "4.12"]
-                 [instaparse "1.4.9"]
-                 [endophile "0.2.1"]])
+                 [instaparse "1.4.9"]])
